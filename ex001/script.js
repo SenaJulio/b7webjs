c2c2b1039cf275a6910f62e63bdc157238f0729b
JULIO SENA <gm.sena@hotmail.com>
2023-12-01 15:32:14 -0300

    Inicio das aulas

---
+++ ex001/script.js
@@ -0,0 +1,16 @@
+let x = 2;
+
+x = x + 8 ;
+console.log(x)
+
+let nome = 'Bonieky';
+let sobrenome = 'lacerda';
+
+let nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+let n1 = 15;
+let n2 = 20;
+
+let soma = n1 + n2
+console.log(soma)
---
+++ ex002/assets/script.js
@@ -0,0 +1,21 @@
+let nome ='Bonieky Sena'// string
+let idade = 90 // number
+let logado = false //boolean
+let lista =["ovo","massa","corante"] // array
+let nomeCompleto ={ nome: "Bonieky", idade:90} // object
+ let carro = "Ferrari"
+ let bolo = 19.99
+ let cidade = "São Paulo"
+ 
+console.log(cidade)
+console.log(bolo)
+console.log(carro)
+console.log(nome)
+console.log(idade)
+console.log(lista)
+console.log(nomeCompleto)
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof logado)
+console.log(typeof lista)
+console.log(typeof nomeCompleto)
