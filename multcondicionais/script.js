a7d9802c13d9a6a07dafdaef27ba5fc006ebbc2c
JULIO SENA <gm.sena@hotmail.com>
2023-12-03 12:29:19 -0300

    Condicional

---
+++ multcondicionais/script.js
@@ -0,0 +1,53 @@
+let idade = 61;
+
+/*
+if (idade>=18){
+    if (idade < 60) {
+        console.log('você é um adulto');
+    }
+}
+*/
+/*
+if(idade >= 18 && idade <60){
+    console.log('você é um adulto')
+}
+
+/*
+&& (e) todas verdadeira
+// (ou) somente uma verdadeira
+
+*/
+/* condicional dupla
+
+else if várias condicionais juntas
+
+*/
+if (idade < 18) {
+    console.log('você é uma criança');
+}else if (idade >= 18 && idade < 60){
+        console.log('você é um adulto.');
+    }else if(idade >= 60){ 
+        console.log('você é um idoso')
+    
+}
+
+let x = 10;
+let y = 5;
+console.log( x > 5);
+
+let w = '10';
+let z = 10;
+console.log( w === z);
+
+let a = 10;
+let b = 5;
+console.log(a > b);
+
+let preco = 40.3;
+if(preco < 45){
+    console.log('preco da carne está barato')
+    }else if(preco > 45){
+        console.log('preco da carne está Caro')
+    }
+   
+
