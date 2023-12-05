c952d909a5023ed568669218974f878affcf81c4
JULIO SENA <gm.sena@hotmail.com>
2023-12-05 16:59:40 -0300

    retorno da função

--- intruçãoFunçao/script.js
+++ intruçãoFunçao/script.js
@@ -1,1 +1,30 @@
 // o que é uma função?
+
+function gravidade() {  // criação da função
+    console.log('A gravidade do planeta é:');
+    console.log(9.8);
+}
+
+gravidade() // chama a função ou uso da função
+
+function somar(n1, n2) { // () porta de entrada da função
+ let resultado = n1 + n2
+ console.log('Resultado:' + resultado)
+}
+somar(10, 15);
+
+function nomeCompleto(nome, sobrenome){
+    console.log(`${nome} ${sobrenome}`);
+}
+nomeCompleto('Bonieky', 'Lacerda');
+nomeCompleto('João', 'Silva'); // passar paramentros
+
+// returno da função
+// ENTRADA -> PRECESSAMENTO -> SAIDA
+
+function nomeCompleto(nome, sobrenome){
+    return `${nome} ${sobrenome}`
+    
+}
+let completo = nomeCompleto('Bonieky', 'Lacerda');
+console.log('completo:' +  completo);
