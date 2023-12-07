cbf20313e9a5317a510e58c690d35141cbd4b89d
JULIO SENA <gm.sena@hotmail.com>
2023-12-07 15:03:30 -0300

    exercicio função

--- intruçaoexercicios/script.js
+++ intruçaoexercicios/script.js
@@ -8,14 +8,37 @@ function calcPct(x, y) {
 }
 console.log(`${pct}% de ${x} é ${y}`)
 
+/*
+calcule o preco do imóvel
+- m2 = 3000
+- Se tiver 1 quartos, o m2 é 1x
+- se tiver 2 quaros , o m2 é 1.2x
+- se tiver 3 quartos, o m2 é 1.5x
+ uso da função
+*/
 
-let calcularImovel = ' metragemQuartos'
+function calcularImovel(metragem, quartos) {
 
-switch (claculsrImovel) {
-    case value:
-        
+    let m2 = 3000;
+    let preco = 0;
+
+    switch (quartos) {
+        case 1 :
+            default:
+                preco = metragem * m2;
+            break;
+            case 2:
+                preco = metragem * (m2 * 1.2);
         break;
-
-    default:
+             case 3:
+                 preco = metragem * (m2 * 1.5);
         break;
-}
+      }  
+
+    return preco;
+    }
+    let metragem = 123
+    let quartos = 3
+    let preco = calcularImovel(metragem, quartos);
+   console.log(`A casa custa R$ ${preco}`)
+
