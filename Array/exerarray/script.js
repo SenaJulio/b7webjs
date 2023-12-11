3c254c6e7ea41ca432b48e16ac0019d94d569789
JULIO SENA <gm.sena@hotmail.com>
2023-12-11 11:34:50 -0300

    Exercicios Array

---
+++ Array/exerarray/script.js
@@ -0,0 +1,11 @@
+let ingredientes = [
+    'água',
+    'farinha',
+    'ovo',
+    'corante',
+    'sal'
+
+]
+ingredientes[5]= 'cebola'
+console.log(`Total de ingredientes: ${ingredientes.length}`);
+//length = quantidade de itens no array
---
+++ Array/script.js
@@ -0,0 +1,20 @@
+let colors = ['blue', 'red', 'green']
+console.log(colors);
+// console,log(colors[0]) Exibir apenas o valor de cada item
+
+let idades = [10,20,30];
+console.log(idades);
+
+let lista = ['algo', 30, 'blabla', true]
+
+let nomes = ['bonieky', 'pedro', 'silvia']
+console.log(nomes);
+
+let listas = ['blabla', nomes]
+console.log(listas);
+let lista2 = ['blabla',['xx','yy']]
+
+
+let segundo = lista[1]
+console.log(lista2[1][0]);
+console.log(segundo[0]);
