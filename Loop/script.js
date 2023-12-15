ef383ba47cf2de94f5c63c4315464e99768ce600
JULIO SENA <gm.sena@hotmail.com>
2023-12-15 12:32:38 -0300

    Loop for

--- Loop/script.js
+++ Loop/script.js
@@ -2,3 +2,37 @@
 for(let n = 0;n < 10; n++){
     console.log('Frase Qualquer' + n);
 }
+
+for (let n = 2; n < 20; n++) {
+ console.log("Contando sempre" + n);
+}
+
+
+// loop em array
+
+let cores = ['preto', 'branco', 'azul', 'vermelho']
+
+for(let n = 0; n < cores.length; n++){
+    console.log(cores[n]);
+}
+// um pouco mais inteligente
+
+for(let i in cores){
+    console.log(cores[i]);
+}
+for( let cor of cores){
+    console.log(cor);
+}
+
+let coress = [
+    {nome: 'preto', qt:10},
+    {nome: 'azul', qt:5},
+    {nome: 'vermelho', qt:15}
+]
+for(let cor of coress){
+    console.log(`Nome: ${cor.nome} - ${cor.qt}`);
+}
+for( let i in coress){
+    coress[i].nome = coress[i].nome.toUpperCase();
+}
+console.log(coress);
