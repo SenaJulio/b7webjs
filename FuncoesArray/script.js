7ed61cbd60ecddafb92f5c1035b51306c6cbc97d
JULIO SENA <gm.sena@hotmail.com>
2023-12-17 16:46:30 -0300

    objetos

--- FuncoesArray/script.js
+++
@@ -1,9 +0,0 @@
-let fruits = ['Maça', 'Uva', 'Laranja', ' Banana'];
-
-fruits.push('Kiwi') // acrescenta um item
-fruits.pop(); // exclui o ultimo item
-fruits.shift() // exclui o primeiro item
-fruits.join('') // retornar uma string
-
-console.log(fruits);
-console.log(fruits.length);
