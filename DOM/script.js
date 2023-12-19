de57b84e0812bdb1a8e9a9bae261eae22540b863
JULIO SENA <gm.sena@hotmail.com>
2023-12-19 15:37:54 -0300

    Evento de clique

---
+++ DOM/script.js
@@ -0,0 +1,6 @@
+document.getElementById('teste')
+document.getElementsByClassName('.botao')
+document.getElementsByTagName('h1')
+
+document.querySelector('#teste')
+document.querySelectorAll('teste2')
---
+++ Eventos de clique/script.js
@@ -0,0 +1,21 @@
+function clicou() {
+    console.log("Clicou no botao");
+}
+
+// alternativa 2
+document.querySelector('.botao');
+botao.addEventListener('click',clicou)
+botao.addEventListener('click',function(){
+    clicou()
+})
+botao.addEventListener('click',() =>{
+    clicou();
+});
+
+document.querySelector('h1');
+h1.addEventListener('font-size')
+document.querySelector()
+addEventListener()
+// Listener = escutador
+
+// addEventListener = adicione um escudador de eventos
