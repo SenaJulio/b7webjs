521e742ad69f4a65c642613be72cd944f8406584
JULIO SENA <gm.sena@hotmail.com>
2024-01-03 17:42:43 -0300

    construtor e this

---
+++ Modulo4Objetos/#1 o que é oo/script.js
@@ -0,0 +1,11 @@
+ /*
+ Programação Orientada a objetos (POO - OOp)
+ Programação Procedural
+ Programação Funcional (PF _ FP)  (functionaal programming  paradigm)
+
+ prototype-based procedural language
+
+
+ - CLASSES
+ - FUNÇÕES / OBJETOS
+ */
---
+++ Modulo4Objetos/#3classesContrutorThis/script.js
@@ -0,0 +1,8 @@
+/* criar uma classe de uma pessoa, Um  template */
+
+class Person{
+constructor(name, age){
+    this.name = name;
+    this.age = age;
+}
+}
